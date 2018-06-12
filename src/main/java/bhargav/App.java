914bda4c619ad3693e54705813bb5b318f6344dc
byeddula <byeddula@ltu.edu>
2018-06-12 11:01:47 -0400

    to build

---
+++ src/main/java/bhargav/App.java
@@ -0,0 +1,13 @@
+package bhargav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
